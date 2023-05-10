696b1b9934ee13357b71c2c1bc279bf4bac67dd1
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-05-10 14:53:44 -0300

    feat[Funcionamento Inicial]: Mecanica principal, divisao por salas de 2 jogadores

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const plays = ["pedra", "papel", "tesoura"];
+const plays = ["Pedra", "Papel", "Tesoura"];
 
 app.use(express.static(__dirname + "/public"));
 
@@ -14,50 +14,106 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Armazena as salas e os jogadores
+const rooms = {};
+
 // Inicializa o servidor WebSocket
 io.on("connection", (socket) => {
   console.log(`Novo jogador conectado: ${socket.id}`);
 
+  // Cria uma nova sala e adiciona o jogador
+  let room = null;
+  for (const roomId in rooms) {
+    if (rooms[roomId].players.length < 2) {
+      room = roomId;
+      break;
+    }
+  }
+
+  if (!room) {
+    room = socket.id;
+    rooms[room] = { players: [socket.id] };
+  } else {
+    rooms[room].players.push(socket.id);
+  }
+
   // Envia as jogadas disponíveis para o novo jogador
   socket.emit("plays", plays);
 
-  // Quando um jogador faz uma jogada
-  socket.on("play", (play) => {
-    console.log(`Jogada do jogador ${socket.id}: ${play}`);
+// Função para determinar o resultado do jogo
+function determineResult(player, opponent) {
+  if (player === opponent) {
+    return "Empate!";
+  } else if (
+    (player === "Pedra" && opponent === "Tesoura") ||
+    (player === "Papel" && opponent === "Pedra") ||
+    (player === "Tesoura" && opponent === "Papel")
+  ) {
+    return "Você ganhou!";
+  } else {
+    return "Você perdeu!";
+  }
+}
 
-    // Escolhe uma jogada aleatória para o oponente
-    const opponentPlay = plays[Math.floor(Math.random() * plays.length)];
+// Quando um jogador faz uma jogada
+socket.on("play", (play) => {
+  console.log(`Jogada do jogador ${socket.id} na sala ${room}: ${play}`);
 
-    let result = "";
-    if (play === opponentPlay) {
-      result = "Empate!";
-    } else if (
-      (play === "pedra" && opponentPlay === "tesoura") ||
-      (play === "papel" && opponentPlay === "pedra") ||
-      (play === "tesoura" && opponentPlay === "papel")
-    ) {
-      result = "Você ganhou!";
-    } else {
-      result = "Você perdeu!";
+  const opponent = rooms[room].players.find((player) => player !== socket.id);
+  if (!opponent) {
+    return;
+  }
+
+  // Inicializa a estrutura de jogadas para a sala, se ainda não estiver inicializada
+  if (!rooms[room].plays) {
+    rooms[room].plays = {};
+  }
+
+  // Armazena a jogada do jogador
+  rooms[room].plays[socket.id] = play;
+
+  // Verifica se ambos os jogadores já fizeram suas jogadas
+  const players = Object.keys(rooms[room].plays);
+  if (players.length === 2) {
+    const player1 = players[0];
+    const player2 = players[1];
+
+    const player1Play = rooms[room].plays[player1];
+    const player2Play = rooms[room].plays[player2];
+
+    // Verifica o resultado do jogo
+    const resultPlayer1 = determineResult(player1Play, player2Play);
+    const resultPlayer2 = determineResult(player2Play, player1Play);
+
+    // Envia o resultado para cada jogador
+    io.to(player1).emit("result", { player: player1Play, opponent: player2Play, result: resultPlayer1 });
+    io.to(player2).emit("result", { player: player2Play, opponent: player1Play, result: resultPlayer2 });
+
+    // Limpa as jogadas da sala
+    delete rooms[room].plays[player1];
+    delete rooms[room].plays[player2];
+  }
+});
+
+
+  // Quando um jogador se desconecta
+  socket.on("disconnect", () => {
+    console.log(`Jogador desconectado: ${socket.id}`);
+
+    // Remove o jogador da sala
+    const playerIndex = rooms[room].players.indexOf(socket.id);
+    if (playerIndex !== -1) {
+      rooms[room].players.splice(playerIndex, 1);
     }
 
-    // Envia o resultado para o jogador
-    socket.emit("result", {
-      player: { id: socket.id, play },
-      opponent: { play: opponentPlay },
-      result,
-    });
+    // Se não houver mais jogadores na sala, remove a sala
+    if (rooms[room].players.length === 0) {
+      delete rooms[room];
+    }
   });
 });
 
-// Iniciar o servidor
-const PORT = 80;
-
-server.listen(PORT, () => {
-	if (PORT == 80) {
-		console.log(`Servidor rodando na porta ${PORT} http://localhost`);
-	} else {
-		console.log(`Servidor rodando na porta ${PORT} http://localhost:${PORT}`);
-	}
+const port = 80;
+server.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
--- public/script.js
+++ public/script.js
@@ -9,7 +9,51 @@ let result = null;
 const socket = io();
 socket.on("plays", (availablePlays) => {
     plays = availablePlays;
+    draw();
 });
+
+socket.on("result", (data) => {
+    playerPlay = data.player;
+    opponentPlay = data.opponent;
+    result = determineResult(playerPlay, opponentPlay);
+    draw();
+});
+
+function determineResult(player, opponent) {
+    if (player === opponent) {
+        return "Empate!";
+    } else if (
+        (player === "Pedra" && opponent === "Tesoura") ||
+        (player === "Papel" && opponent === "Pedra") ||
+        (player === "Tesoura" && opponent === "Papel")
+    ) {
+        return "Você ganhou!";
+    } else {
+        return "Você perdeu!";
+    }
+}
+
+function draw() {
+    const playSize = canvas.width / 3;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < plays.length; i++) {
+        context.fillStyle = "#DDD";
+        context.fillRect(i * playSize, 0, playSize, playSize);
+        context.fillStyle = "black";
+        context.font = "48px Arial";
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillText(plays[i], i * playSize + playSize / 2, playSize / 2);
+    }
+
+    if (playerPlay !== null && opponentPlay !== null && result !== null) {
+        context.font = "24px Arial";
+        context.textAlign = "center";
+        context.fillText(`Você: ${playerPlay} | Oponente: ${opponentPlay}`, canvas.width / 2, playSize * 2);
+        context.fillText(result, canvas.width / 2, playSize * 3);
+    }
+}
 
 canvas.addEventListener("click", (event) => {
     if (playerPlay !== null) {
@@ -31,33 +75,4 @@ canvas.addEventListener("click", (event) => {
     socket.emit("play", playerPlay);
 });
 
-socket.on("result", (data) => {
-    playerPlay = data.player.play;
-    opponentPlay = data.opponent.play;
-    result = data.result;
-});
-
-function draw() {
-    const playSize = canvas.width / 3;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (playerPlay !== null && opponentPlay !== null && result !== null) {
-        context.font = "24px Arial";
-        context.textAlign = "center";
-        context.fillText(`Você: ${playerPlay} | Oponente: ${opponentPlay}`, canvas.width / 2, playSize * 2);
-        context.fillText(result, canvas.width / 2, playSize * 3);
-        return;
-    }
-
-    for (let i = 0; i < plays.length; i++) {
-        context.fillStyle = "#DDD";
-        context.fillRect(i * playSize, 0, playSize, playSize);
-        context.fillStyle = "black";
-        context.font = "48px Arial";
-        context.textAlign = "center";
-        context.textBaseline = "middle";
-        context.fillText(plays[i], i * playSize + playSize / 2, playSize / 2);
-    }
-}
-
-setInterval(draw, 100);
+draw();
